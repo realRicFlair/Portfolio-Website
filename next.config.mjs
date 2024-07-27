8dd6fe38dbfb25c02dd654713a15cd18d80209e6
realRicFlair <realRicFlair@hotmail.com>
2024-07-27 13:49:25 -0700

    reverted nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {
-  output: "export", // enables static exports
-  reactStrictMode: true,
-};
+const nextConfig = {};
 
 export default nextConfig;
