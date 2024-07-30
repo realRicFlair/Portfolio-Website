92ab05b38ff3011d2ab77ed9cfac61f84c3b60b9
realRicFlair <realRicFlair@hotmail.com>
2024-07-29 18:41:55 -0700

    added insight thing

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
- 
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 export default function RootLayout({
   children,
 }: {
@@ -8,6 +9,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <SpeedInsights />
       <body className={`${inter.className} antialiased`}>
         <div className="px-4 md:px-8 lg:px-16 xl:px-32">{children}</div>
       </body>
